6cc6c2a742c0ea1912f3dace9b974db67305db91
Austin Zhang <austinz2@illinois.edu>
2019-03-14 13:03:46 -0500

    Works with multiple messages now.. maybe

--- imagesavebot.py
+++ imagesavebot.py
@@ -1,6 +1,6 @@
-CONFIG_DEFAULT = {"PRESERVE_IMAGES": True,
-          "IMAGE_TARGET_CHANNEL": 361662909220388876,
-          "LOGGING_LEVEL" : "INFO"}
+CONFIG_DEFAULT = {"PRESERVE_IMAGES"     : True,
+                  "IMAGE_TARGET_CHANNEL": 361662909220388876,
+                  "LOGGING_LEVEL"       : "INFO"}
 
 import collections
 import logging
@@ -8,10 +8,7 @@ from io import BytesIO
 import discord
 import requests
 import lux
-
-
-
-
+import itertools
 
 BOT_NAME = "IMGBOT"
 
@@ -34,12 +31,11 @@ async def on_message(message_in):
     return
 
 @client.event
-async def on_reaction_add(reaction : discord.Reaction, user : discord.User):
+async def on_reaction_add(reaction: discord.Reaction, user: discord.User):
     if reaction.message.channel.id == CONFIG["IMAGE_TARGET_CHANNEL"]:
         if reaction.count > 1 and reaction.emoji == "❌":
             await reaction.message.delete()
     pass
-
 
 async def copy_attachments(message: discord.Message):
     if not CONFIG["PRESERVE_IMAGES"]:
@@ -54,11 +50,13 @@ async def copy_attachments(message: discord.Message):
             response = requests.get(attachment.url)
             files.append(discord.File(BytesIO(response.content), filename="image.png"))
 
-    message = await client.get_channel(CONFIG["IMAGE_TARGET_CHANNEL"]).send(
-        f"Copying images from `UID: [{message.author.id}]` {message.author.mention} in `CID: [{message.channel.id}]` `({message.channel.name})` <#{message.channel.id}>, sourced from message `MID: [{message.id}]` {message.jump_url}",
-        files=files)
+    for index, file in enumerate(files):
+        message = await client.get_channel(CONFIG["IMAGE_TARGET_CHANNEL"]).send(
+            f"Copying images from `UID: [{message.author.id}]` {message.author.mention} in "
+            f"`CID: [{message.channel.id}]` `({message.channel.name})` <#{message.channel.id}>, "
+            f"sourced from message `MID: [{message.id}]` {message.jump_url} "
+            f"{str(index) + '/' + str(len(files)) if len(files) > 1 else ''}",
+            files=file)
     await message.add_reaction("❌")
-
-
 
 client.run(CONFIG.TOKEN, bot=True)
